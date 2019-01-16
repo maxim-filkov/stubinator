8f567399b1eff1bc671ad6f2fd91f6669cd59d91
Max Filkov <maximfilkov@gmail.com>
2019-01-15 22:42:18 -0500

    Updated to support routing for stubbed requests instead of exposing the stubbed end points dynamically.

--- app.rb
+++ app.rb
@@ -2,49 +2,144 @@ require 'sinatra/base'
 
 class Stubinator < Sinatra::Base
 
+  @@my_routes = []
+
   before do
     Dir.glob("responses/*.json") { |path|
-      json = JSON.parse(File.read(path))
-      self.class.superclass.send(json["method"], json["path"]) do
-        unless json["headers"].nil?
-          json["headers"].each { |k, v| headers[k] = v }
-        end
-        unless json["status"].nil?
-          status json["status"].to_i
-        end
-        json["body"] ? json["body"] : ""
-      end
+      stub = JSON.parse(File.read(path))
+      @@my_routes << stub
     }
   end
 
   post '/stub' do
-    body = request.body.read
-    json = JSON.parse(body)
-    if json['path'].nil? or json['method'].nil?
-      status 400
-      headers['Content-Type'] = 'application/json'
-      body json['path'].nil? ? '{"error": "empty \'path\' field is not allowed"}' : '{"error": "empty \'method\' field is not allowed"}'
-    else
-      File.write("responses/#{params[:name]}.json", body)
-      restart_app
+    begin
+      body = request.body.read
+      json = JSON.parse(body)
+      if json['name'].nil? or json['path'].nil? or json['method'].nil?
+        field = json['name'].nil? ? 'name' : json['path'].nil? ? 'path' : 'method'
+        response_with(400, "{\"error\":\"the '#{field}' field is required\"}")
+      else
+        add_stub({ 'name' => json["name"], 'path' => json["path"], 'method' => json['method'],
+                   'status' => json["status"], 'body' => json["body"], 'headers' => json["headers"] })
+        body ''
+      end
+    rescue JSON::ParserError
+      response_with(400, '{"error":"the body should represent a stub in valid JSON format"}')
     end
   end
 
   delete '/stub' do
-    name = params[:name]
-    File.delete("responses/#{name}.json")
-    restart_app
+    json = {}
+    begin
+      json = JSON.parse(request.body.read)
+      if json['name'].nil?
+        response_with(400, '{"error":"the \'name\' field is required"}')
+      else
+        File.delete("responses/#{json['name']}.json")
+      end
+    rescue JSON::ParserError
+      response_with(400, '{"error":"the body should represent a stub in valid JSON format"}')
+    end
   end
 
   patch '/stub' do
+    update = {}
+    begin
+      update = JSON.parse(request.body.read)
+      if update['name'].nil?
+        response_with(400, '{"error":"the \'name\' field is required"}')
+      else
+        if File.exist?("responses/#{update['name']}.json")
+          stub = JSON.parse(File.read("responses/#{update['name']}.json"))
+          begin
+            stub_body_as_json = JSON.parse(stub['body'])
+            body_as_json = JSON.parse(update['body'])
+          rescue Exception
+            # ignored
+          end
+          unless update['headers'].nil?
+            stub['headers'] = update['headers'].merge(update['headers'])
+          end
+          unless update['status'].nil?
+            stub['status'] = update['status']
+          end
+          unless update['path'].nil?
+            stub['path'] = update['path']
+          end
+          unless update['method'].nil?
+            stub['method'] = update['method']
+          end
+          unless update['body'].nil?
+            if stub_body_as_json.nil? and body_as_json.nil?
+              stub['body'] = update['body']
+            else
+              stub['body'] = stub_body_as_json.merge(body_as_json).to_json
+            end
+          end
+          add_stub(stub)
+          body ''
+        else
+          response_with(400, "{\"error\": \"stub with name '#{json['name']}' not found\"}")
+        end
+      end
+    rescue JSON::ParserError
+      response_with(400, '{"error":"the body should represent a stub in valid JSON format"}')
+    end
+  end
 
+  head '/*' do
+    route('head', params[:splat].first)
+  end
+
+  get '/*' do
+    route('get', params[:splat].first)
+  end
+
+  post '/*' do
+    route('post', params[:splat].first)
+  end
+
+  put '/*' do
+    route('put', params[:splat].first)
+  end
+
+  patch '/*' do
+    route('patch', params[:splat].first)
+  end
+
+  delete '/*' do
+    route('delete', params[:splat].first)
+  end
+
+  options '/*' do
+    route('options', params[:splat].first)
   end
 
   private
 
-  def restart_app
-    app_root = `passenger-status | grep 'App root' | sed 's/^.*: //'`
-    system("passenger-config restart-app #{app_root}")
+  def add_stub(stub)
+    @@my_routes.delete_if { |r| r['name'] == stub['name'] }
+    @@my_routes << stub
+    File.write("responses/#{stub["name"]}.json", stub.to_json)
+  end
+
+  def route(method, path)
+    # puts "method: #{method}, path: #{path}"
+    # puts @@my_routes.inspect
+    route = @@my_routes.select { |r| r['path'] == "/#{path}" and r['method'] == method }.first
+    if route.nil?
+      response_with(404, "{\"error\": \"the stub '#{method.upcase!} /#{path}' hasn't been found\"}")
+    else
+      status route['status']
+      route['headers'].each { |k, v| headers[k] = v }
+      body route['body']
+    end
+  end
+
+  def response_with(status, message)
+    status status
+    headers['Content-Type'] = 'application/json'
+    body message
   end
 
 end
--- spec/stubinator_spec.rb
+++ spec/stubinator_spec.rb
@@ -13,88 +13,196 @@ class HelloWorldTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def test_possible_to_stub_get
-    when_add_endpoint_stub(@stub_name = 'test_get', 'get', '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_get')
+    when_create_stub('test_get', 'get', '200', { test_header_key: 'test_header_value' }, 'body', '/test_get')
     when_send_request('get', '/test_get')
     then_response_is_equal_to(200, 'body', { test_header_key: 'test_header_value' })
   end
 
   def test_possible_to_stub_post
-    when_add_endpoint_stub(@stub_name = 'tes_post', 'post', '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/tes_post')
+    when_create_stub('tes_post', 'post', '200', { test_header_key: 'test_header_value' }, 'body', '/tes_post')
     when_send_request('post', '/tes_post')
     then_response_is_equal_to(200, 'body', { test_header_key: 'test_header_value' })
   end
 
   def test_possible_to_stub_delete
-    when_add_endpoint_stub(@stub_name = 'test_delete', 'delete', '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_delete')
+    when_create_stub('test_delete', 'delete', '200', { test_header_key: 'test_header_value' }, 'body', '/test_delete')
     when_send_request('delete', '/test_delete')
     then_response_is_equal_to(200, 'body', { test_header_key: 'test_header_value' })
+    # then_stub_file_is_deleted('test_delete')
   end
 
   def test_possible_to_stub_put
-    when_add_endpoint_stub(@stub_name = 'test_put', 'put', '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_put')
+    when_create_stub('test_put', 'put', '200', { test_header_key: 'test_header_value' }, 'body', '/test_put')
     when_send_request('put', '/test_put')
     then_response_is_equal_to(200, 'body', { test_header_key: 'test_header_value' })
   end
 
   def test_possible_to_stub_head
-    when_add_endpoint_stub(@stub_name = 'test_head', 'head', '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_head')
+    when_create_stub('test_head', 'head', '200', { test_header_key: 'test_header_value' }, 'body', '/test_head')
     when_send_request('head', '/test_head')
     then_response_is_equal_to(200, '', { test_header_key: 'test_header_value' })
   end
 
   def test_possible_to_stub_options
-    when_add_endpoint_stub(@stub_name = 'test_options', 'options', '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_options')
+    when_create_stub('test_options', 'options', '200', { test_header_key: 'test_header_value' }, 'body', '/test_options')
     when_send_request('options', '/test_options')
     then_response_is_equal_to(200, 'body', { test_header_key: 'test_header_value' })
   end
 
   def test_get_stubbing_with_no_body
-    when_add_endpoint_stub(@stub_name = 'test_no_body', 'get', '200',
-                           { test_header_key: 'test_header_value' }, nil, '/test_no_body')
+    when_create_stub('test_no_body', 'get', '200', { test_header_key: 'test_header_value' }, nil, '/test_no_body')
     when_send_request('get', '/test_no_body')
     then_response_is_equal_to(200, '', { test_header_key: 'test_header_value' })
   end
 
   def test_stubbing_with_no_status
-    when_add_endpoint_stub(@stub_name = 'test_no_status', 'get', nil,
-                           { test_header_key: 'test_header_value' }, 'body', '/test_no_status')
+    when_create_stub('test_no_status', 'get', nil, { test_header_key: 'test_header_value' }, 'body', '/test_no_status')
     when_send_request('get', '/test_no_status')
     then_response_is_equal_to(200, 'body', { test_header_key: 'test_header_value' })
   end
 
   def test_stubbing_with_no_path
-    when_add_endpoint_stub(@stub_name = 'test_no_path', 'get', '200',
-                           { test_header_key: 'test_header_value' }, 'body', nil)
-    then_response_is_equal_to(400, '{"error": "empty \'path\' field is not allowed"}', { 'Content-Type': 'application/json' })
+    when_create_stub('test_no_path', 'get', '200', { test_header_key: 'test_header_value' }, 'body', nil)
+    then_response_is_equal_to(400, '{"error":"the \'path\' field is required"}',
+                              { 'Content-Type': 'application/json' })
   end
 
   def test_stubbing_with_no_method
-    when_add_endpoint_stub(@stub_name = 'test_no_method', nil, '200',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_no_method')
-    then_response_is_equal_to(400, '{"error": "empty \'method\' field is not allowed"}', { 'Content-Type': 'application/json' })
+    when_create_stub('test_no_method', nil, '200', { test_header_key: 'test_header_value' }, 'body', '/test_no_method')
+    then_response_is_equal_to(400, '{"error":"the \'method\' field is required"}',
+                              { 'Content-Type': 'application/json' })
   end
 
   def test_stubbing_with_404_status
-    when_add_endpoint_stub(@stub_name = 'test_404_status', 'get', '404',
-                           { test_header_key: 'test_header_value' }, 'body', '/test_404_status')
+    when_create_stub('test_404_status', 'get', '404', { test_header_key: 'test_header_value' }, 'body', '/test_404_status')
     when_send_request('get', '/test_404_status')
     then_response_is_equal_to(404, 'body', { test_header_key: 'test_header_value' })
   end
 
-  teardown do
-    delete_endpoint_stub(@stub_name)
+  def test_update_stub_method
+    given_endpoint_stub(name: 'test_method_update',
+                        method: 'get',
+                        status: 200,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: '{"key":"value"}',
+                        path: '/test_method_update')
+    when_update_stub(name: 'test_method_update', method: 'post')
+    when_send_request('post', '/test_method_update')
+    then_response_is_equal_to(200, '{"key":"value"}', { 'Content-Type': 'application/json' })
+  end
+
+  def test_update_stub_status
+    given_endpoint_stub(name: 'test_status_update',
+                        method: 'get',
+                        status: 200,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: '{"key":"value"}',
+                        path: '/test_status_update')
+    when_update_stub(name: 'test_status_update', status: 502)
+    when_update_stub(name: 'test_status_update', status: 502)
+    when_send_request('get', '/test_status_update')
+    then_response_is_equal_to(502, '{"key":"value"}', { 'Content-Type': 'application/json' })
+  end
+
+  def test_update_stub_path
+    given_endpoint_stub(name: 'test_path_update',
+                        method: 'get',
+                        status: 200,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: '{"key":"value"}',
+                        path: '/test_path_update_before')
+    when_update_stub(name: 'test_path_update', path: '/test_status_update_after')
+    when_send_request('get', '/test_status_update_after')
+    then_response_is_equal_to(200, '{"key":"value"}', { 'Content-Type': 'application/json' })
+  end
+
+  def test_update_stub_headers
+    given_endpoint_stub(name: 'test_headers_update',
+                        method: 'get',
+                        status: 200,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: '{"key":"value"}',
+                        path: '/test_headers_update')
+    when_update_stub(name: 'test_headers_update', headers: { 'new_header': 'foo' })
+    when_send_request('get', '/test_headers_update')
+    then_response_is_equal_to(200, '{"key":"value"}', { 'new_header': 'foo' })
+  end
+
+  def test_update_stub_body
+    given_endpoint_stub(name: 'test_body_update',
+                        method: 'get',
+                        status: 200,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: '{"key1":"value1_before","key2":"value2"}',
+                        path: '/test_body_update')
+    when_update_stub(name: 'test_body_update', body: '{"key1":"value1_after"}')
+    when_send_request('get', '/test_body_update')
+    then_response_is_equal_to(200, '{"key1":"value1_after","key2":"value2"}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+  def test_update_nested_json
+    given_endpoint_stub(name: 'test_nested_json_update',
+                        method: 'get',
+                        status: 200,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: '{"key":{"nested_key_before":"nested_value_before"}}',
+                        path: '/test_nested_json_update')
+    when_update_stub(name: 'test_nested_json_update', body: '{"key":{"nested_key_after":"nested_value_after"}}')
+    when_send_request('get', '/test_nested_json_update')
+    then_response_is_equal_to(200, '{"key":{"nested_key_after":"nested_value_after"}}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+  def test_create_stub_no_name
+    post "/stub", '{"key":"value"}'
+    then_response_is_equal_to(400, '{"error":"the \'name\' field is required"}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+
+  def test_update_stub_no_name
+    patch "/stub", '{"key":"value"}'
+    then_response_is_equal_to(400, '{"error":"the \'name\' field is required"}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+  def test_delete_stub_no_name
+    delete "/stub", '{}'
+    then_response_is_equal_to(400, '{"error":"the \'name\' field is required"}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+  def test_create_stub_non_json
+    post "/stub"
+    then_response_is_equal_to(400, '{"error":"the body should represent a stub in valid JSON format"}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+  def test_patch_stub_non_json
+    patch "/stub"
+    then_response_is_equal_to(400, '{"error":"the body should represent a stub in valid JSON format"}',
+                              { 'Content-Type': 'application/json' })
+  end
+
+  def test_delete_stub_non_json
+    delete "/stub"
+    then_response_is_equal_to(400, '{"error":"the body should represent a stub in valid JSON format"}',
+                              { 'Content-Type': 'application/json' })
   end
 
   private
 
-  def when_add_endpoint_stub(name, method, status, headers, body, path)
-    create_endpoint_stub(name, method, status, headers, body, path)
+  def given_endpoint_stub(name:, method:, status: nil, headers: {}, body: '', path: nil)
+    create_stub(name, method, status, headers, body, path)
+  end
+
+  def when_create_stub(name, method, status, headers, body, path)
+    create_stub(name, method, status, headers, body, path)
+  end
+
+  def when_update_stub(name:, path: nil, method: nil, status: nil, headers: nil, body: nil)
+    update_stub(name, method, status, headers, body, path)
   end
 
   def when_send_request(method, path)
@@ -105,17 +213,17 @@ class HelloWorldTest < Test::Unit::TestCase
     assert_equal(status, last_response.status)
     assert_equal(body, last_response.body)
     headers.each do |key, value|
-      assert(last_response.headers.has_key?(key.to_s))
+      assert(last_response.headers.has_key?(key.to_s), "the header with key '#{key}' wasn't found")
       assert_equal(value, last_response.headers[key.to_s])
     end
   end
 
-  def delete_endpoint_stub(name)
-    File.delete("responses/#{name}.json") if File.exist?("responses/#{name}.json")
-    sleep 3
+  teardown do
+    delete_stub(@stub_name)
   end
 
-  def create_endpoint_stub(name, method, status, headers, body, path)
+  def create_stub(name, method, status, headers, body, path)
+    @stub_name = name
     payload = { method: method, name: name }
     unless status.nil?
       payload["status"] = status
@@ -129,7 +237,33 @@ class HelloWorldTest < Test::Unit::TestCase
     unless path.nil?
       payload["path"] = path
     end
-    post "/stub?name=#{name}", payload.to_json
+    post "/stub", payload.to_json
+    sleep 2
+  end
+
+  def update_stub(name, method, status, headers, body, path)
+    payload = { name: name }
+    unless method.nil?
+      payload["method"] = method
+    end
+    unless status.nil?
+      payload["status"] = status
+    end
+    unless headers.nil?
+      payload["headers"] = headers
+    end
+    unless body.nil?
+      payload["body"] = body
+    end
+    unless path.nil?
+      payload["path"] = path
+    end
+    patch "/stub", payload.to_json
+    sleep 2
+  end
+
+  def delete_stub(name)
+    File.delete("responses/#{name}.json") if File.exist?("responses/#{name}.json")
     sleep 3
   end
 
